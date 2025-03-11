78ba95ad105833e726b4ece7282603d94561ac76
Thiago Santos <Thiago_S_Sousa@outlook.com>
2025-03-10 21:16:35 -0300

    feat: responsive navbar menu added, banner layout changes applied

---
+++ main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const navbarToggle = document.getElementById("navbar");
+  const closeButton = document.querySelector("nav .xmark");
+  const menu = document.getElementById("menu");
+
+  function toggleNavbar() {
+    menu.classList.toggle("active");
+  }
+
+  navbarToggle.addEventListener("click", toggleNavbar);
+  closeButton.addEventListener("click", toggleNavbar);
+});
